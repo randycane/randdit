e9e8338c0e82b87914bc0cb69ea072684664b130
Randy <randychang97@gmail.com>
2022-12-04 21:10:54 -0800

    target blank to new tab

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -5,20 +5,22 @@ function FooterComponent() {
         <footer className="whole-foot">
             <div className="feet">
                 <div className="re-got">
-                    Get to know me!
+                    Developed by Randy Chang
                 </div>
                 <div className="about-me">
                     <a
                         className="git-me-contained"
                         href="https://github.com/randycane"
+                        target="_blank"
                         >
                         <div className="re-got"> My Github:
                         <i class="fa-brands fa-github"></i>
                         </div>
                             </a>
-                    <a
+                    <a _blank
                         className="linked-in-contained"
                         href="https://www.linkedin.com/in/randy-y-chang/"
+                        target="_blank"
                         >
                         <div className="size">
                                 <div className="re-got">
